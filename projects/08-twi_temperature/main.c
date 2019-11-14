cd6a110f046138c5ba50eaedfc16c3eb5d6c36bb
lab661 <lab661@PC-445.urel.feec.vutbr.cz>
2019-11-14 14:58:28 +0100

    fixes and updates

--- projects/08-twi_temperature/main.c
+++ projects/08-twi_temperature/main.c
@@ -18,32 +18,11 @@
 #include "timer.h"
 #include "uart.h"
 #include "twi.h"
+#include <stdio.h>
 
-
-
-/* Typedef -----------------------------------------------------------*/
-typedef enum {
-    IDLE_STATE = 1,
-    TRY_STATE,
-    ACK_STATE
-} state_t;
-
-/* Define ------------------------------------------------------------*/
 #define UART_BAUD_RATE 9600
 
-/* Variables ---------------------------------------------------------*/
-state_t current_state = IDLE_STATE;
 
-/* Function prototypes -----------------------------------------------*/
-void fsm_twi_scanner(void);
-
-/* Functions ---------------------------------------------------------*/
-/**
- *  Brief:  Main program. Test all TWI slave addresses and scan 
- *          connected devices.
- *  Input:  None
- *  Return: None
- */
 int main(void)
 {
     // UART: asynchronous, 8-bit data, no parity, 1-bit stop
@@ -78,162 +57,38 @@ int main(void)
  */
 ISR(TIMER1_OVF_vect)
 {
-   // fsm_twi_scanner();
-    
-    // uint8_t h_int=0;
-    // uint8_t h_fract=0;
-    // uint8_t t_int=0;
-    // uint8_t t_fract=0;
-
-    uint8_t seconds=0;
-   // uint8_t minutes=0;
-    uint8_t hours=0;
-    uint8_t sec=0;
-    
-    // char H_int[3];
-    // char H_fract[3];
-    // char T_int[3];
-    // char T_fract[3];
-
-    char RTC_seconds[3];
-    char RTC_minutes[3];
-    char RTC_hours[3];
    
-    //uint8_t ht_addr = 0x5c; 
-    uint8_t rtc_addr = 0x68; 
+    uint8_t h_int=0;
+    uint8_t h_fract=0;
+    uint8_t t_int=0;
+    uint8_t t_fract=0;
+
+    char string1 [10];
+
+    uint8_t ht_addr = 0x5c; 
+
     uint8_t response;
-    uint8_t shift=0;
-    uint8_t shift1=0;
-
-
-    
-    uint8_t bitmasc = 0b01110000;
-    uint8_t bitmasc2= 0b00001111;
+   
    
 
-    // response = twi_start((ht_addr<<1) + TWI_WRITE);
+    response = twi_start((ht_addr<<1) + TWI_WRITE);
         
-    //     if(response == 0)
-    //     {
-    //         twi_writbitmasce(0x00);
-    //     }bitmasc
-    //     twi_stop();bitmasc
-
-    //     response = tbitmascwi_start((ht_addr<<1) + TWI_READ);
-    //     if(response bitmasc== 0)
-    //     {
-    //         h_int =     twi_read_ack();
-    //         h_fract =   twi_read_ack();
-    //         t_int =     twi_read_ack();
-    //         t_fract =   twi_read_nack();
-    //     }    
-    //     twi_stop();
-    
-    response = twi_start((rtc_addr<<1) + TWI_WRITE);
-        
-        if(response == 0){
-
-           twi_write(0x00);
+        if(response == 0)
+        {
+            twi_write(0x00);
         }
         twi_stop();
 
-    response = twi_start((rtc_addr<<1) + TWI_READ);
-       
-        if(response == 0)
+        response = twi_start((ht_addr<<1) + TWI_READ);
+        if(response== 0)
         {
-            seconds = twi_read_ack();
-
-        shift = seconds&bitmasc;
-        shift =(shift>>4);
-        shift1=seconds&bitmasc2;
-        sec=(10*shift)+shift1;
-
-           // minutes = twi_read_ack();          
-            hours =   twi_read_nack();
+            h_int =     twi_read_ack();
+            h_fract =   twi_read_ack();
+            t_int =     twi_read_ack();
+            t_fract =   twi_read_nack();
         }    
         twi_stop();
 
-    itoa(sec, RTC_seconds, 10);
-    itoa(sec, RTC_minutes, 10);
-    itoa(hours, RTC_hours, 10);
-     
-     
-    // itoa(h_int, H_int, 10);
-    // itoa(h_fract, H_fract, 10);
-    // itoa(t_int, T_int, 10);
-    // itoa(t_fract, T_fract, 10);
-     
-    //uart_puts(RTC_hours);
-    //uart_puts(":"); 
-    //uart_puts(RTC_minutes);
-    
-    uart_puts(RTC_seconds);
-    uart_puts("\r\n"); 
-
-
-
-    //  uart_puts("HUM: "); 
-    //  uart_puts(H_int);
-    //  uart_puts("."); 
-    //  uart_puts(H_fract);
-    //  uart_puts("   TEMP: "); 
-    //  uart_puts(T_int);
-    //  uart_puts("."); 
-    //  uart_puts(T_fract);
-    //  uart_puts("\r\n"); 
-      
-
-}
-
-/**
- *  Brief: Test one TWI address.
- */
-void fsm_twi_scanner(void)
-{
-    static uint8_t addr = 0;
-    uint8_t status;
-    char uart_string[3];
-    uint8_t i = 1;
-
-    switch (current_state) {
-    case IDLE_STATE:
-        if (addr < 128) {
-            itoa(addr, uart_string, 16);
-            uart_puts("\r\n");
-            uart_puts(uart_string);
-            current_state = TRY_STATE;
-        }
-        break;
-
-    // Transmit TWI slave address and check status
-    case TRY_STATE:
-        status = twi_start((addr<<1) + TWI_WRITE);
-        twi_stop();
-
-        if (status == 0) {
-            current_state = ACK_STATE;
-        } else {
-            addr++;
-            current_state = IDLE_STATE;
-            uart_puts(" NOTHING");
-        }
-        break;
-
-    // Slave ACK received
-    case ACK_STATE:
-        //uart_puts(" <-ADDRESS OF SLAVE DETECTED");
-        
-        i=twi_start(57);
-         if (i==0){
-             uart_puts("Score");
-         }
-         else
-         uart_puts("no responce");
-        addr++;
-        current_state = IDLE_STATE;
-        break;
-
-    default:
-        current_state = IDLE_STATE;
-    }
+    sprintf(string, "H %02d.%02d T:%02d.%02d \r\n", h_int, h_fract, t_int, t_fract);
+    uart_puts(string1);
 }
--- projects/10_test/main.c
+++ projects/10_test/main.c
@@ -3,16 +3,21 @@
 #include "timer.h"
 #include "uart.h"
 #include "twi.h"
+#include <stdio.h>
 
 
 #define UART_BAUD_RATE 9600
+
+//static int FILE mystduot = FDEV_SETUP_STREAM(lcd_putchar, NULL, _FDEV_SETUP_WRITE);
 
 int main(void)
 {
 
   uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
 
-  TIM_config_prescaler(TIM1, TIM_PRESC_8);
+  twi_init();
+
+  TIM_config_prescaler(TIM1, TIM_PRESC_256);
   TIM_config_interrupt(TIM1, TIM_OVERFLOW_ENABLE);
 
   sei();
@@ -29,20 +34,66 @@ int main(void)
 ISR(TIMER1_OVF_vect)
 {   
   
-  uint8_t byte_sec = 0b01010100;
-  uint8_t byte_min = 0b00110000;
-  uint8_t byte_hour = 0b00010001;
+  uint8_t byte_sec = 0;
+  uint8_t byte_min = 0;
+  uint8_t byte_hour = 0;
+  uint8_t rtc_addr = 0x68;
+  uint8_t ht_addr = 0x5c;
+  uint8_t response;
 
-  char string [3];
+  uint8_t h_int=0;
+  uint8_t h_fract=0;
+  uint8_t t_int=0;
+  uint8_t t_fract=0;
+
+response = twi_start((rtc_addr<<1) + TWI_WRITE);
+        
+  if(response == 0)
+  {
+    twi_write(0x00);
+  }
+  twi_stop();
+
+  response = twi_start((rtc_addr<<1) + TWI_READ);
+       
+  if(response == 0)
+  {
+    byte_sec = twi_read_ack();
+    byte_min = twi_read_ack();      
+    byte_hour = twi_read_nack();
+  }    
+  twi_stop();
+  
 
   uint8_t seconds=(((byte_sec&~0b00001111)>>4)*10+(byte_sec&~0b01110000));
   uint8_t minutes=(((byte_min&~0b00001111)>>4)*10+(byte_min&~0b01110000));
   uint8_t hours=(((byte_hour&~0b00001111)>>4)*10+(byte_hour&~0b01110000));
 
-  uart_puts(itoa(hours, string, 10));
-  uart_puts(":");
-  uart_puts(itoa(minutes, string, 10));
-  uart_puts(":");
-  uart_puts(itoa(seconds, string, 10));
-  uart_puts("\r\n");
+  char string1 [20];
+  sprintf(string1, "%02d:%02d:%02d   ",hours, minutes, seconds);
+  
+   response = twi_start((ht_addr<<1) + TWI_WRITE);
+        
+    if(response == 0)
+      {
+      twi_write(0x00);
+      }
+      twi_stop();
+  response = twi_start((ht_addr<<1) + TWI_READ);
+    
+    if(response== 0)
+       {
+          h_int =     twi_read_ack();
+          h_fract =   twi_read_ack();
+          t_int =     twi_read_ack();
+          t_fract =   twi_read_nack();
+      }    
+      twi_stop();
+
+  char string [15];
+  sprintf(string, "H: %d.%d T: %d.%d C \r\n", h_int, h_fract, t_int, t_fract);
+  
+  uart_puts(string1);  
+  uart_puts(string);
+
 }
